7d798c482e53cc8fda333f70ab9ad8dce266c198
Ngoc <dungtuyetngoc@gmail.com>
2023-11-29 18:46:42 +0700

    fix conflict for sidebar

--- src/main/java/Client/Admin/Views/Sidebar.java
+++ src/main/java/Client/Admin/Views/Sidebar.java
@@ -11,21 +11,24 @@ public class Sidebar extends Box {
 
     public Sidebar() {
         super(BoxLayout.Y_AXIS);
-        setPreferredSize(new Dimension(256, 0));
+        setPreferredSize(new Dimension(220, 0));
         setBackground(Color.gray);
         add(Box.createRigidArea(new Dimension(0, 10)));
         String[] menuItems = { "User list", "Login list", "Group chat list",
                 "Report list" };
         String[] icons = {"user.png", "login.png", "groupchat.png", "report.png"};
         for (int i = 0; i < 4; i++) {
+            JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+            panel.setOpaque(false); // panel transparent
             JButton button = new JButton(menuItems[i]);
+            button.setForeground(Color.white);
             button.setMaximumSize(new Dimension(Integer.MAX_VALUE, button.getMinimumSize().height));
             button.setFont(new Font("Segoe UI Variable Text", Font.BOLD, 20));
             button.setBorderPainted(false);
             button.setFocusPainted(false);
             button.setContentAreaFilled(false);
             button.setCursor(new Cursor(Cursor.HAND_CURSOR)); // Change cursor to hand cursor when hovering over the
-                                                              // button
+
             button.setHorizontalAlignment(SwingConstants.LEFT); // Align text to the left
             ImageIcon icon = new ImageIcon(getClass().getResource("/Image/" + icons[i]));
             Image img = icon.getImage().getScaledInstance(20, 20, java.awt.Image.SCALE_SMOOTH);
@@ -33,6 +36,9 @@ public class Sidebar extends Box {
             button.setIcon(icon);
             button.setIconTextGap(16);
             add(button);
+
+            setMaximumSize(new Dimension(Integer.MAX_VALUE, button.getPreferredSize().height)); // Set panel's maximum size to fit button
+
             buttons.add(button);
 
             // Add space between buttons
@@ -40,6 +46,9 @@ public class Sidebar extends Box {
                 add(Box.createRigidArea(new Dimension(0, 20)));
             }
         }
+
+        setBackground(new Color(0x36598E));
+        setOpaque(true);
     }
 
     public JButton getButton(int index) {
