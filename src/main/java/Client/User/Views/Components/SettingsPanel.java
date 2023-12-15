9f04571b89ac21bd4fd673186889b125a3254443
Ngoc <dungtuyetngoc@gmail.com>
2023-12-15 16:46:06 +0700

    added ui for right panel

--- src/main/java/Client/User/Views/Components/SettingsPanel.java
+++ src/main/java/Client/User/Views/Components/SettingsPanel.java
@@ -1,21 +1,92 @@
 package Client.User.Views.Components;
 
+import Client.User.Views.Util;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class SettingsPanel extends JPanel {
-//    private JTabbedPane settingsTabbedPane;
     public SettingsPanel() {
-        super(new BorderLayout());
-//        settingsTabbedPane = new JTabbedPane();
-        setBorder(BorderFactory.createTitledBorder("Settings"));
+        super(new BorderLayout(0, 15));
         setPreferredSize(new Dimension(300, this.getPreferredSize().height));
 
-//        add(settingsTabbedPane, BorderLayout.CENTER);
+        // Create add user button
+        JButton addUserButton = new JButton();
+        ImageIcon addIcon = Util.createRoundedImageIcon("add-user.png", 50);
+        addUserButton.setIcon(addIcon);
+        addUserButton.setPreferredSize(new Dimension(addIcon.getIconWidth(), addIcon.getIconHeight()));
 
-        // Add tabs to the settings panel
-//        settingsTabbedPane.addTab("General", new JPanel());
-//        settingsTabbedPane.addTab("Notifications", new JPanel());
+        // Create a panel to hold the buttons
+        JPanel topPanel = new JPanel();
+        topPanel.add(addUserButton);
 
+        JPanel bottomPanel = new JPanel();
+        bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.Y_AXIS));
+
+        String[] buttonTexts = {"Unfriend", "Block", "Report Spam", "Delete History"};
+
+        for (String buttonText : buttonTexts) {
+            HoverablePanel textPanel = new HoverablePanel();
+
+            // Set the preferred height of the button based on the text height
+            int preferredHeight = getFontMetrics(textPanel.getFont()).getHeight() + 10; // gets text's height + 10 is for padding
+            textPanel.setPreferredSize(new Dimension(Integer.MAX_VALUE, preferredHeight));
+            textPanel.setMaximumSize(new Dimension(Integer.MAX_VALUE, preferredHeight));
+
+            textPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+            bottomPanel.add(textPanel);
+
+            JLabel label = new JLabel(buttonText);
+            label.setHorizontalAlignment(JLabel.CENTER);
+            textPanel.add(label);
+
+            bottomPanel.add(Box.createVerticalStrut(5)); // set vertical gap between buttons
+        }
+
+        // Add the button panel to the settings panel
+        add(topPanel, BorderLayout.NORTH);
+        add(bottomPanel, BorderLayout.CENTER);
+    }
+
+    private static class HoverablePanel extends JPanel {
+        private boolean hovered = false;
+
+        public HoverablePanel() {
+            addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseEntered(MouseEvent e) {
+                    hovered = true;
+                    repaint();
+                }
+
+                @Override
+                public void mouseExited(MouseEvent e) {
+                    hovered = false;
+                    repaint();
+                }
+            });
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            if (hovered) {
+                g.setColor(new Color(173, 216, 230)); // Light blue color
+                g.fillRect(0, 0, getWidth(), getHeight());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Settings Panel");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.getContentPane().add(new SettingsPanel());
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        });
     }
 }
